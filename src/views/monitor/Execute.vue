40558d883da282e6e05e90d4ba73c7f5efde7a2a
Hank Shen <swhbox@foxmail.com>
2021-06-21 11:08:34 +0800

    update

--- src/views/monitor/Execute.vue
+++ src/views/monitor/Execute.vue
@@ -1,11 +1,10 @@
 <template>
   <section>
-
-      <el-card class="box-card">
+    <el-card class="box-card">
   <div slot="header" class="clearfix">
     <span>任务快照列表</span>
   </div>
-  <div  class="text item">
+  <div class="text item">
     <!--工具条-->
     <el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
       <el-form :inline="true" :model="filters">
@@ -15,9 +14,8 @@
          <el-form-item>
           <el-input v-model="filters.name" placeholder="请输入任务名称"></el-input>
         </el-form-item>
-          <el-form-item>
-
-<el-select v-model="filters.status" clearable placeholder="请选择执行状态" style="width:100%;">
+        <el-form-item>
+        <el-select v-model="filters.status" clearable placeholder="请选择执行状态" style="width:100%;">
           <el-option
             v-for="item in status"
             :key="item.value"
@@ -39,8 +37,6 @@
          <el-form-item>
           <el-input v-model="filters.ip" placeholder="请输入ip"></el-input>
         </el-form-item>
-
-
         <el-form-item>
           <el-button type="primary" @click="getSnapshots">查询</el-button>
         </el-form-item>
@@ -59,7 +55,6 @@
          <el-table-column prop="ip" label="ip" width="150" >
         </el-table-column>
         <el-table-column prop="status" label="执行状态" width="100">
-
             <template slot-scope="scope">
               <el-tag type="success" size="mini" v-if="scope.row.status==1">EXECUTING</el-tag>
               <el-tag type="primary" size="mini" v-else-if="scope.row.status==2">COMPLETE</el-tag>
@@ -67,16 +62,14 @@
                <el-tag type="warning" size="mini" v-else-if="scope.row.status==3">UNKNOW</el-tag>
           </template>
         </el-table-column>
-         <el-table-column  label="耗时" width="100" >
-
+         <el-table-column label="耗时" width="100" >
             <template slot-scope="scope">
               <el-tag type="danger" size="mini">{{scope.row.times}}秒</el-tag>
           </template>
         </el-table-column>
         <el-table-column prop="target" label="Target" width="250" >
         </el-table-column>
-
-           <el-table-column prop="createTime" label="创建时间" width="200" >
+        <el-table-column prop="createTime" label="创建时间" width="200" >
         </el-table-column>
         <el-table-column prop="startTime" label="开始时间" width="200" >
         </el-table-column>
@@ -94,8 +87,7 @@
 
        <!--工具条-->
     <el-col :span="24" class="toolbar">
-      <el-pagination layout="total,sizes,prev, pager, next"  @size-change="handleSizeChange" @current-change="handleCurrentChange" :page-size="pageSize"  :page-sizes="[20, 30, 50, 100]" :total="total"
-                     style="float:right;">
+      <el-pagination layout="total,sizes,prev, pager, next"  @size-change="handleSizeChange" @current-change="handleCurrentChange" :page-size="pageSize"  :page-sizes="[20, 30, 50, 100]" :total="total" style="float:right;">
       </el-pagination>
     </el-col>
   </div>
@@ -106,68 +98,64 @@
     <el-dialog title="任务快照详情" :visible.sync="viewFormVisible" :close-on-click-modal="false">
       <el-form :model="viewForm" label-width="100px"  ref="viewForm">
           <el-form-item label="快照Id" prop="id">
-          <el-input v-model="viewForm.id" auto-complete="off" disabled></el-input>
+          <el-input v-model="viewForm.id" disabled></el-input>
         </el-form-item>
             <el-form-item label="任务Id" prop="jobId">
-          <el-input v-model="viewForm.jobId" auto-complete="off" disabled></el-input>
+          <el-input v-model="viewForm.jobId" disabled></el-input>
         </el-form-item>
         <el-form-item label="任务名称" prop="name">
-          <el-input v-model="viewForm.name" auto-complete="off" disabled></el-input>
+          <el-input v-model="viewForm.name" disabled></el-input>
         </el-form-item>
         <el-form-item label="任务集群" prop="group">
-             <el-input v-model="viewForm.group" auto-complete="off" disabled></el-input>
+             <el-input v-model="viewForm.group" disabled></el-input>
         </el-form-item>
         <el-form-item label="Cron表达式" prop="cron" >
-             <el-input v-model="viewForm.cron" auto-complete="off"  disabled></el-input>
+             <el-input v-model="viewForm.cron" disabled></el-input>
         </el-form-item>
           <el-form-item label="ip" prop="cron" >
-             <el-input v-model="viewForm.ip" auto-complete="off"  disabled></el-input>
+             <el-input v-model="viewForm.ip" disabled></el-input>
         </el-form-item>
          <el-form-item label="目标任务" prop="target">
-            <el-input v-model="viewForm.target"  auto-complete="off" disabled></el-input>
+            <el-input v-model="viewForm.target" disabled></el-input>
         </el-form-item>
          <el-form-item label="任务参数" prop="params">
-            <el-input v-model="viewForm.params"  auto-complete="off" disabled></el-input>
+            <el-input type="textarea" :autosize="{ minRows: 1, maxRows: 20}" v-model="viewForm.params" disabled></el-input>
         </el-form-item>
          <el-form-item label="创建时间" prop="createTime">
-            <el-input v-model="viewForm.createTime"  auto-complete="off" disabled></el-input>
+            <el-input v-model="viewForm.createTime" disabled></el-input>
         </el-form-item>
           <el-form-item label="手机号码" prop="mobile">
-            <el-input v-model="viewForm.mobile"  auto-complete="off" disabled></el-input>
+            <el-input v-model="viewForm.mobile" disabled></el-input>
           </el-form-item>
 
             <el-form-item label="状态" prop="status">
-            <el-input v-model="viewForm.status"  auto-complete="off" disabled></el-input>
+            <el-input v-model="viewForm.status" disabled></el-input>
           </el-form-item>
 
               <el-form-item label="耗时" prop="times">
-            <el-input v-model="viewForm.times"  auto-complete="off" disabled></el-input>
+            <el-input v-model="viewForm.times" disabled></el-input>
           </el-form-item>
             <el-form-item label="创建时间" prop="createTime">
-            <el-input v-model="viewForm.createTime"  auto-complete="off" disabled></el-input>
+            <el-input v-model="viewForm.createTime" disabled></el-input>
           </el-form-item>
               <el-form-item label="开始时间" prop="startTime">
-            <el-input v-model="viewForm.startTime"  auto-complete="off" disabled></el-input>
+            <el-input v-model="viewForm.startTime" disabled></el-input>
           </el-form-item>
 
         <el-form-item label="完成时间" prop="finishTime">
-            <el-input v-model="viewForm.finishTime"  auto-complete="off" disabled></el-input>
+            <el-input v-model="viewForm.finishTime" disabled></el-input>
           </el-form-item>
             <el-form-item label="备注" prop="remark">
-            <el-input v-model="viewForm.remark"  auto-complete="off" disabled></el-input>
+            <el-input type="textarea" :autosize="{ minRows: 1, maxRows: 20}" v-model="viewForm.remark" disabled></el-input>
           </el-form-item>
            <el-form-item label="结果" prop="result">
-            <el-input v-model="viewForm.result"  auto-complete="off" disabled></el-input>
+            <el-input type="textarea" :autosize="{ minRows: 1, maxRows: 20}" v-model="viewForm.result" disabled></el-input>
           </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click.native="viewFormVisible = false">关闭</el-button>
       </div>
     </el-dialog>
-
-
-
-
   </section>
 </template>
 <script>
@@ -179,9 +167,7 @@ export default {
             id:'',
             group:'',
             ip:''
-
         },
-
         status:[
             {"name":"EXECUTING","value":1},
             {"name":"COMPLETE","value":2},
@@ -209,11 +195,8 @@ handleSizeChange(size ){
     this.getSnapshots()
 },
       handleCurrentChange(va){
-
           this.page =va
           this.getSnapshots()
-
-
       },
    handleView(index, row){
      this.viewForm = Object.assign({}, row)
@@ -221,7 +204,6 @@ handleSizeChange(size ){
 
    },
    handleDelete(index, row){
-
      this.$confirm('确认要删除此任务快照信息吗？', '友情提示', {}).then(() => {
            let para = Object.assign({}, row)
             snapshotDelete(para).then((res) => {
@@ -237,7 +219,6 @@ handleSizeChange(size ){
    },
     // 获取任务执行快照列表
     getSnapshots: function () {
-
       let para =Object.assign({"pageNo":this.page,"pageSize":this.pageSize}, this.filters)
       this.loading = true
       executeSnapshotList(para).then((res) => {
@@ -247,7 +228,6 @@ handleSizeChange(size ){
       })
     },
     groupList(){
-
       let para = {}
       groupConfList(para).then((res) => {
         this.groups = res.data
